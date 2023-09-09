54b6517d9900ea5bff5d2f095bb638e00aa4f552
Anton <90580311+beliberda@users.noreply.github.com>
2023-09-09 16:38:38 +1000

    Create print.py
    Файл с описанием команды print в питоне

---
+++ print.py
@@ -0,0 +1,17 @@
+print('Первая строчка',sep=' пробел ',end='/')
+print('Вторая строка')
+print(56*1000)
+number = 10
+print(number)
+name = 'Medoded'
+print('Мой никнейм',name,'текст','еще текст', number)
+print(f'Мой никнейм {name} текст еще текст {number}')
+a = 100
+b = 200
+print('Сложение',a+b)
+print('Вычитание',a-b)
+print('Умножение',a*b)
+print('Деление',a/b)
+print('Целочисленное деление',a//b)
+print('Остаток от деления',a%b)
+
